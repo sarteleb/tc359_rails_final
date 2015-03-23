c9ed9f762cb1fb5a886c8d40432cd4ce8718a28f
sarteleb <sarteleb@gmail.com>
2015-03-23 12:16:31 -0400

    trying somethign

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,6 @@ module BrandonsApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # config/application.rb
-    config.assets.initialize_on_precompile = false
+    
   end
 end
